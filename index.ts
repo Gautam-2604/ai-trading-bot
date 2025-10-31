84e1b7545f5ddabe3f98c55de89d31a112471da8
Gautam-2604 <gaurisariag@gmail.com>
2025-11-01 01:01:29 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import { CandlestickApi, IsomorphicFetchHttpLibrary, ServerConfiguration } from "./lighter-sdk-ts/generated";
+const BASE_URL = "https://mainnet.zklighter.elliot.ai"
+const SOL_MARKET_ID = 2
+
+async function getKlines(marketId: number) {
+    const klinesApi = new CandlestickApi({
+        baseServer: new ServerConfiguration<{  }>(BASE_URL, {  }),
+        httpApi: new IsomorphicFetchHttpLibrary(),
+        middleware: [],
+        authMethods: {}
+    });
+
+    const klines = await klinesApi.candlesticks(SOL_MARKET_ID, '1m', Date.now() - 1000 * 60 * 60 * 1, Date.now(), 50, false);
+    const midPrices = klines.candlesticks.slice(-10).map(({open, close})=>
+        (open+close)/2
+    )
+    console.log(midPrices);
+}
+
+getKlines(SOL_MARKET_ID);
